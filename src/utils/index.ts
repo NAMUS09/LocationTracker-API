29087d8d2cb8def9dc5b89f422c4d512f9216ad6
Suman Shrestha <shresthasuman839@gmail.com>
2024-03-16 16:46:06 +0530

    import  updated

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,9 @@
-export { default as logger } from "./logger";
-export { default as getText } from "./lang/get-text";
+export { default as logger } from "./logger.js";
+export { default as getText } from "./lang/get-text.js";
 export {
   generateAccessAndRefereshTokens,
   removeToken,
-} from "./helpers/jwt-token-helper";
-export { default as ipHelper } from "./helpers/ip-helper";
-export { default as errorHelper } from "./helpers/error-helper";
-export { default as generateRandomCode } from "./helpers/generate-random-code";
+} from "./helpers/jwt-token-helper.js";
+export { default as ipHelper } from "./helpers/ip-helper.js";
+export { default as errorHelper } from "./helpers/error-helper.js";
+export { default as generateRandomCode } from "./helpers/generate-random-code.js";
