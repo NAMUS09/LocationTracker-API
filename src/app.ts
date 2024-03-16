ec09a7400161d27ea248861aa9914314c630c366
Suman Shrestha <shresthasuman839@gmail.com>
2024-03-16 16:05:27 +0530

    vercel.json updated

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import { port } from "./config/index.js";
-import loader from "./loaders/index.js";
+import { port } from "./config/index";
+import loader from "./loaders/index";
 
 const app = express();
 
