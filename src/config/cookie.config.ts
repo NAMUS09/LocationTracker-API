1cd3f0cb16d25cfeea432a3f63ce14da67baa1d7
Suman Shrestha <shresthasuman839@gmail.com>
2024-03-16 18:39:27 +0530

    cookie options changed

--- src/config/cookie.config.ts
+++ src/config/cookie.config.ts
@@ -10,8 +10,9 @@ export const cookieAccessToken = "accessToken";
 export const cookieRefreshToken = "refreshToken";
 
 export const cookieClientOptions: CookieOptions = {
+  domain: ".vercel.app",
   httpOnly: false,
   secure: true,
-  sameSite: true,
+  sameSite: "none",
 };
 export const cookieClient = "user";
