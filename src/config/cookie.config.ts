6ad02c3efb86d50e1995c2eaea3dc201a70b3b9a
Suman Shrestha <shresthasuman839@gmail.com>
2024-03-16 21:21:50 +0530

    config updated

--- src/config/cookie.config.ts
+++ src/config/cookie.config.ts
@@ -2,6 +2,7 @@ import { CookieOptions } from "express";
 
 const { DOMAIN } = process.env;
 export const cookieOptions: CookieOptions = {
+  domain: DOMAIN,
   httpOnly: true,
   secure: true,
   sameSite: "none",
@@ -11,6 +12,7 @@ export const cookieAccessToken = "accessToken";
 export const cookieRefreshToken = "refreshToken";
 
 export const cookieClientOptions: CookieOptions = {
+  domain: DOMAIN,
   httpOnly: false,
   secure: true,
   sameSite: "none",
